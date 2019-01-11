6df0985f0a227aea08c9dc92eb675cb9a3f3a690
LegendSteve <46540640+LegendSteve@users.noreply.github.com>
2019-01-11 19:36:55 +0100

    Update gmxrandom.js

--- gmxrandom.js
+++ gmxrandom.js
@@ -1,14 +1,9 @@
 var lsg = function(name) {return localStorage.getItem(name)};
 
-function randomvorhanden(vorname,nachname){
+function randomvorhanden(){
     var inputs = document.getElementsByTagName("inputs");
 
-    for(i = 0; i < inputs.length; i++){
-        var input = inputs[i];
-        
-        switch(input.getAttribute("data-test")){
-            case "first-name-input": input.value = vorname; break;
-            case "last-name-input": input.value = nachname; break;
-        }
-    }
+    console.log(inputs);
+
+    inputs[3].value = "Hallo";
 }
