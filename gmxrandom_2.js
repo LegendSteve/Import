8e3843f6eb091776f7deccae49b71187966de174
LegendSteve <46540640+LegendSteve@users.noreply.github.com>
2019-01-11 18:05:05 +0100

    Update gmxrandom_2.js

--- gmxrandom_2.js
+++ gmxrandom_2.js
@@ -5,14 +5,6 @@ function randomvorhanden(vorname,nachname){
 
     for(i = 0; i < inputs.length; i++){
         var input = inputs[i];
-
-        if(input.getAttribute("data-test") == "first-name-input"){
-            input.value = "hallo";
-        }
-
-        switch(input.getAttribute("data-test")){
-            case "first-name-input": input.value = lsg("vorname"); break;
-            case "last-name-input": input.value = lsg("nachname"); break;
-        }
+        input.value = "hallo";
     }
 }
